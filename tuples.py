65aa63ad2a9780542c0bd8a9484352daa2af4c3f
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-05-29 17:20:10 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,39 +1,46 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
 
-
+azara_list = [
+    ("Amethyst Octopus", "1F"),
+    ("Angry Monkey Figurine", "5B"),
+    ("Antique Glass Fishnet Float", "3D"),
+    ("Brass Spyglass", "4B"),
+    ("Carved Wooden Elephant", "8C"),
+    ("Crystal Crab", "6A"),
+    ("Glass Starfish", "6D"),
+    ("Model Ship in Large Bottle", "8A"),
+    ("Pirate Flag", "7F"),
+    ("Robot Parrot", "1C"),
+    ("Scrimshawed Whale Tooth", "2A"),
+    ("Silver Seahorse", "4E"),
+    ("Vintage Pirate Hat", "7E")
+]
+rui_list = [
+    ("Seaside Cottages", ("1", "C"), "Blue"),
+    ("Aqua Lagoon (Island of Mystery)", ("1", "F"), "Yellow"),
+    ("Deserted Docks", ("2", "A"), "Blue"),
+    ("Spiky Rocks", ("3", "D"), "Yellow"),
+    ("Abandoned Lighthouse", ("4", "B"), "Blue"),
+    ("Hidden Spring (Island of Mystery)", ("4", "E"), "Yellow"),
+    ("Stormy Breakwater", ("5", "B"), "Purple"),
+    ("Old Schooner", ("6", "A"), "Purple"),
+    ("Tangled Seaweed Patch", ("6", "D"), "Orange"),
+    ("Quiet Inlet (Island of Mystery)", ("7", "E"), "Orange"),
+    ("Windswept Hilltop (Island of Mystery)", ("7", "F"), "Orange"),
+    ("Harbor Managers Office", ("8", "A"), "Purple"),
+    ("Foggy Seacave", ("8", "C"), "Purple")
+]
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
 
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
+    return record[1]
 
-    _, coordinate = record
-    return coordinate
-
-
-def convert_coordinate(coordinate: str):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
+def convert_coordinate(coordinate):
 
     return (coordinate[0], coordinate[1])
 
-
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
 
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-    
-    az_tesoro, az_coordenada = azara_record
-    rui_ubicación, rui_coordenada, rui_cuadrante = rui_record
-
-    if az_coordenada[0] != rui_coordenada[0] or az_coordenada[1] != rui_coordenada[1] :
+    azara_coordinate = convert_coordinate(azara_record[1])
+    if azara_coordinate == rui_record[1]:
+        return (azara_record[0], azara_record[1], rui_record[0], rui_record[1], rui_record[2])
+    else:
         return "not a match"
-
-    return (az_tesoro, az_coordenada, rui_ubicación, rui_coordenada, rui_cuadrante)
